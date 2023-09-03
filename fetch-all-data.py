a7bf8f3708c8f167b61ba71ebe2feb61d4ab9905
Kevin Veen-Birkenbach <kevin@veen.world>
2023-09-03 20:31:39 +0200

    Refactored code

--- fetch-all-data.py
+++ fetch-all-data.py
@@ -1,41 +1,45 @@
 import requests
 import argparse
 
-def get_all_rows_from_table(base_url, api_key, table_id):
-    headers = {
+def create_headers(api_key):
+    """Create headers for API requests."""
+    return {
         "Authorization": f"Token {api_key}",
         "Content-Type": "application/json"
     }
+
+def handle_api_response(response):
+    """Handle API response, check for errors and decode JSON."""
+    if response.status_code != 200:
+        print(f"Error: Received status code {response.status_code} from Baserow API.")
+        print("Response content:", response.content.decode())
+        return None
+
+    try:
+        return response.json()
+    except requests.RequestsJSONDecodeError:
+        print("Error: Failed to decode the response as JSON.")
+        return None
+
+def get_all_rows_from_table(base_url, api_key, table_id):
+    headers = create_headers(api_key)
     rows = []
     next_url = f"{base_url}database/rows/table/{table_id}/"
 
     while next_url:
         response = requests.get(next_url, headers=headers)
-        data = response.json()
+        data = handle_api_response(response)
+        if not data:
+            break
         rows.extend(data['results'])
         next_url = data['next']
 
     return rows
 
 def get_all_tables_from_database(base_url, api_key, database_id):
-    headers = {
-        "Authorization": f"Token {api_key}",
-        "Content-Type": "application/json"
-    }
+    headers = create_headers(api_key)
     response = requests.get(f"{base_url}database/tables/database/{database_id}/", headers=headers)
-    
-    # Check if the response status code indicates success
-    if response.status_code != 200:
-        print(f"Error: Received status code {response.status_code} from Baserow API.")
-        print("Response content:", response.content.decode())
-        return []
-
-    try:
-        tables = response.json()
-        return tables
-    except requests.RequestsJSONDecodeError:
-        print("Error: Failed to decode the response as JSON.")
-        return []
+    return handle_api_response(response) or []
 
 def get_all_data_from_database(base_url, api_key, database_id):
     tables = get_all_tables_from_database(base_url, api_key, database_id)
