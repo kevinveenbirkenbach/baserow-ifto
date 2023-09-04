e69fb4db04fb78cced778ebd656ff6b2d173f48c
Kevin Veen-Birkenbach <kevin@veen.world>
2023-09-05 01:51:27 +0200

    In between commit matrix optimization

--- matrix_builder.py
+++ matrix_builder.py
@@ -9,9 +9,14 @@ class MatrixBuilder:
             print(message)
             
     def build_multitable_matrix(self, tables_data):
+        original_keys=list(tables_data.keys())
         for table_name, table_rows in tables_data.copy().items():
             self.build_matrix(tables_data,table_name, table_rows)
-        return tables_data
+        
+        matrix={}
+        for key in original_keys:
+            matrix[key]=tables_data[key]
+        return matrix
             
     def build_matrix(self, tables_data, table_name, table_rows, reference_map={}):
         """Build a matrix with linked rows filled recursively."""
@@ -21,11 +26,12 @@ class MatrixBuilder:
 
         return tables_data
 
-    def fill_cells_with_related_content(self,tables_data, table_name, table_rows, reference_map_child):
+    def fill_cells_with_related_content(self, tables_data, table_name, table_rows, reference_map_child):
         """Fill cells with related content."""
         for table_row in table_rows:
             self.print_verbose_message(f"table_row: {table_row}")
             for table_column_name, table_cell_content in table_row.items():
+                self.print_verbose_message(f"table_cell_content {table_cell_content}")
                 if table_column_name in reference_map_child:
                     
                     link_row_table_id = reference_map_child[table_column_name]["link_row_table_id"]
@@ -33,23 +39,24 @@ class MatrixBuilder:
                     
                     link_row_related_field_id = reference_map_child[table_column_name]["link_row_related_field_id"]
                     self.print_verbose_message(f"link_row_related_field_id: {link_row_related_field_id}")
-                    
-                    new_content=[]
-                    for entry_id,entry_content in table_cell_content:
-                        related_cell_identifier=self.generate_related_cell_identifier(link_row_table_id,link_row_related_field_id,entry_id)
-                        self.print_verbose_message(f"related_cell_identifier: {related_cell_identifier}")
+                    # This could be wrong:
+                    if link_row_related_field_id:
+                        new_content = []
+                        for entry_id, entry_content in table_cell_content:
+                            self.print_verbose_message(f"entry_content {entry_content}")
 
-                        if related_cell_identifier in reference_map_child[table_column_name]["embeded"]:
-                            self.print_verbose_message(f"Skipped {related_cell_identifier}. Already implemented")
-                            break
-                        
-                        reference_map_child[table_column_name]["embeded"].append(related_cell_identifier)
-                        matrix_table = self.build_matrix(tables_data, link_row_table_id, tables_data[link_row_table_id], reference_map_child)
-                        new_content.append(matrix_table)
-                    table_row[table_column_name]=new_content
-        return tables_data[tables_data]
-                        
-    
+                            related_cell_identifier = self.generate_related_cell_identifier(link_row_table_id, link_row_related_field_id, entry_id)
+                            self.print_verbose_message(f"related_cell_identifier: {related_cell_identifier}")
+
+                            if related_cell_identifier in reference_map_child[table_column_name]["embeded"]:
+                                self.print_verbose_message(f"Skipped {related_cell_identifier}. Already implemented")
+                                continue
+                                
+                                reference_map_child[table_column_name]["embeded"].append(related_cell_identifier)
+                                self.build_matrix(tables_data, link_row_table_id, tables_data[link_row_table_id], reference_map_child)
+                                new_content.append(entry_content)  # Append the original content or modify as needed
+                        table_row[table_column_name] = new_content
+
     def generate_related_cell_identifier(self, table_id, table_column_id, table_row_id):
         return self.generate_cell_identifier(table_id, "field_" + str(table_column_id), table_row_id);
 
